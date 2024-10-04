682a1601de70d432c52c50be7e78fa0697b4c20f
tristan-orourke <tristan.orourke@gmail.com>
2024-10-04 18:52:37 -0400

    Config tests to connect to db

--- app/config/test.exs
+++ app/config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :photo_tagger, PhotoTagger.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "db",
   database: "photo_tagger_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
