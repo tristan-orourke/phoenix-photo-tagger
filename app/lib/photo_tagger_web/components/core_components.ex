61fcd6c0964ad25b5ccd4c07ca841ae78b208c7a
tristan-orourke <tristan.orourke@gmail.com>
2025-01-02 00:44:31 -0500

    Slightly improve flash msg alert

--- app/lib/photo_tagger_web/components/core_components.ex
+++ app/lib/photo_tagger_web/components/core_components.ex
@@ -115,7 +115,7 @@ defmodule PhotoTaggerWeb.CoreComponents do
       phx-click={JS.push("lv:clear-flash", value: %{key: @kind}) |> hide("##{@id}")}
       role="alert"
       class={[
-        "fixed top-2 right-2 mr-2 w-80 sm:w-96 z-50 rounded-lg p-3 ring-1",
+        "relative w-80 sm:w-96 z-50 rounded-lg p-3 mb-3 ring-1",
         @kind == :info && "bg-emerald-50 text-emerald-800 ring-emerald-500 fill-cyan-900",
         @kind == :error && "bg-rose-50 text-rose-900 shadow-md ring-rose-500 fill-rose-900"
       ]}
@@ -127,7 +127,7 @@ defmodule PhotoTaggerWeb.CoreComponents do
         <%= @title %>
       </p>
       <p class="mt-2 text-sm leading-5"><%= msg %></p>
-      <button type="button" class="group absolute top-1 right-1 p-2" aria-label={gettext("close")}>
+      <button type="button" class="absolute group top-1 right-1 p-2" aria-label={gettext("close")}>
         <.icon name="hero-x-mark-solid" class="h-5 w-5 opacity-40 group-hover:opacity-70" />
       </button>
     </div>
