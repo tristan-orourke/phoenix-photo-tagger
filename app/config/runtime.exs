55c2f60bf8e2cee7676f6428e714aed3cb8ec5d8
tristan-orourke <tristan.orourke@gmail.com>
2025-01-04 02:33:02 -0500

    Tweak prod config

--- app/config/runtime.exs
+++ app/config/runtime.exs
@@ -49,12 +49,13 @@ if config_env() == :prod do
       """
 
   host = System.get_env("PHX_HOST") || "example.com"
+  path = System.get_env("PHX_PATH") || "/"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :photo_tagger, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :photo_tagger, PhotoTaggerWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: 443, scheme: "https", path: path],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
