7d674370a51063afadb415f8db574fd7cf497e3a
tristan-orourke <tristan.orourke@gmail.com>
2025-03-11 23:52:01 -0400

    Wrap tags in photo pane instead of listing vertically

--- app/lib/photo_tagger_web/live/gallery_live/main.ex
+++ app/lib/photo_tagger_web/live/gallery_live/main.ex
@@ -163,9 +163,9 @@ defmodule PhotoTaggerWeb.GalleryLive.Main do
         <.link class={"#{@folder == @photo.folder && "font-bold"}"} patch={build_url(@photo.folder, @photo, @tags)}><%= @photo.folder %></.link>
       </:item>
       <:item title="Tags">
-        <ul>
+        <ul class="flex flex-wrap">
           <%= for tag <- @photo.tags do %>
-            <li >
+            <li class="mr-2">
               <.toggle_tag_button folder={@folder} photo={@photo} tags={@tags} toggled_tag={tag.name}><%= if(tag.name in @tags, do: "- ", else: "+ ") <> tag.name %></.toggle_tag_button>
               <.form class="inline" phx-submit="remove_tag">
                 <input class="hidden" type="text" name="photo_id" value={@photo.id} />
@@ -177,15 +177,19 @@ defmodule PhotoTaggerWeb.GalleryLive.Main do
         </ul>
       </:item>
       <:item title="Add tags">
+        <div class="flex flex-wrap">
         <%= for tag <- @recommended_tags do %>
           <%= if tag not in @photo.tags do %>
-            <.form for={%{"tag" => "", "photo_id" => ""}} phx-submit="add_tag">
-              <input class="hidden" type="text" name="photo_id" value={@photo.id} />
-              <input class="hidden" type="text" name="tag" value={tag.name} />
-              <button class="border border-blue-600 rounded-full hover:bg-blue-100 px-1 my-1 text-blue-600 hover:text-blue-800" type="submit"><%= tag.name %></button>
-            </.form>
+            <div class="mr-2">
+              <.form for={%{"tag" => "", "photo_id" => ""}} phx-submit="add_tag">
+                <input class="hidden" type="text" name="photo_id" value={@photo.id} />
+                <input class="hidden" type="text" name="tag" value={tag.name} />
+                <button class="border border-blue-600 rounded-full hover:bg-blue-100 px-1 my-1 text-blue-600 hover:text-blue-800" type="submit"><%= tag.name %></button>
+              </.form>
+            </div>
           <% end %>
         <% end %>
+        </div>
         <.simple_form :let={f} for={%{"tag" => "", "photo_id" => ""}} phx-submit="add_tag">
           <input class="hidden" type="text" name="photo_id" value={@photo.id} />
           <.input field={f[:tag]} type="text" label="Other" />
