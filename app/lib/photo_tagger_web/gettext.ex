ec4abcd54ec0ece8a23f83c31b2acddba8393aa6
tristan-orourke <tristan.orourke@gmail.com>
2025-03-13 23:55:15 -0400

    Update translator module in line with deprecation warnings

--- app/lib/photo_tagger_web/gettext.ex
+++ app/lib/photo_tagger_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule PhotoTaggerWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :photo_tagger
+  use Gettext.Backend, otp_app: :photo_tagger
 end
