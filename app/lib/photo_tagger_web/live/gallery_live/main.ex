417f086503c343b8fbde5bedeefae1aafe6022b3
tristan-orourke <tristan.orourke@gmail.com>
2025-04-01 01:09:17 -0400

    Ensure photo section doesn't require horizontal scrolling on mobile

--- app/lib/photo_tagger_web/live/gallery_live/main.ex
+++ app/lib/photo_tagger_web/live/gallery_live/main.ex
@@ -586,7 +586,7 @@ defmodule PhotoTaggerWeb.GalleryLive.Main do
         <div class="mt-2">
           <.form for={Component.to_form(%{"tag" => "", "photo_id" => @photo.id})} phx-submit="add_tag">
             <input class="hidden" type="text" name="photo_id" value={@photo.id} />
-            <div class="flex items-center space-x-4">
+            <div class="flex flex-wrap gap-2">
               <%!-- TODO: convert this simple inline form to a component --%>
               <%!-- <.label for="add_any_tag">Add tag</.label> --%>
               <input
@@ -594,7 +594,7 @@ defmodule PhotoTaggerWeb.GalleryLive.Main do
                 name="tag"
                 id="add_any_tag"
                 Placeholder="Add tag"
-                class="block max-w-64 rounded-lg text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6"
+                class="rounded-lg w-full max-w-40 text-zinc-900 focus:ring-0 sm:text-sm sm:leading-6"
               />
               <.button type="submit">Submit</.button>
             </div>
