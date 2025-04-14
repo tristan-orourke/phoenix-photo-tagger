085613a6a6f4eb15d53dbf7f3e50388252b023d6
tristan-orourke <tristan.orourke@gmail.com>
2025-04-14 17:57:09 -0400

    Add example infinite-scroll stream code

--- app/lib/photo_tagger_web/live/gallery_live/gallery_panel.ex
+++ app/lib/photo_tagger_web/live/gallery_live/gallery_panel.ex
@@ -11,16 +11,28 @@ defmodule PhotoTaggerWeb.GalleryLive.GalleryPanel do
   def render(assigns) do
     ~H"""
     <div class="p-2 lg:p-6 ">
-      <ul class={"grid gap-2 lg:gap-4
-      #{@grid_size}
-      md:#{@md_grid_size}
-      lg:#{@lg_grid_size}
-      xl:#{@xl_grid_size}
-      2xl:#{@_2xl_grid_size}"}>
-        <%= for photo <- @photos do %>
+      <ul
+        id="photos-infinite-scroll"
+        phx-update="stream"
+        phx-viewport-top={@page > 1 && "prev-page"}
+        phx-viewport-bottom={!@end_of_timeline? && "next-page"}
+        phx-target={@myself}
+        phx-page-loading
+        class={[
+          "grid gap-2 lg:gap-4",
+          "#{@grid_size}",
+          "md:#{@md_grid_size}",
+          "lg:#{@lg_grid_size}",
+          "xl:#{@xl_grid_size}",
+          "2xl:#{@_2xl_grid_size}",
+          if(@end_of_timeline?, do: "pb-10", else: "pb-[calc(200vh)]"),
+          if(@page == 1, do: "pt-10", else: "pt-[calc(200vh)]")
+        ]}
+      >
+        <%= for {id, photo} <- @streams.photos do %>
           <.live_component
             module={PhotoTaggerWeb.GalleryLive.GalleryPhoto}
-            id={photo.id}
+            id={id}
             photo_id={photo.id}
             photo_group={photo.group}
             photo_name={photo.name}
@@ -32,8 +44,16 @@ defmodule PhotoTaggerWeb.GalleryLive.GalleryPanel do
           />
         <% end %>
       </ul>
+      <div id="infinite-scroll-marker" phx-hook="InfiniteScroll" data-page={@page}></div>
     </div>
     """
+  end
+
+  def mount(socket) do
+    {:ok, socket
+      |> assign(page: 1, per_page: 20)
+      |> assign(photos: nil)
+    }
   end
 
   def update(assigns, socket) do
@@ -68,7 +88,13 @@ defmodule PhotoTaggerWeb.GalleryLive.GalleryPanel do
     {:ok,
      socket
      |> assign(:selected_photo_ids, assigns.selected_photo_ids)
-     |> assign(:photos, assigns.photos)
+     |> assign(:all_photos, assigns.photos)
+     |> then(fn socket ->
+       case socket.assigns.photos do
+         nil -> paginate_photos(socket, 1) # We only want to do this in handle_params the first time
+         _ -> socket
+       end
+     end)
      |> assign(:collapse_groups, assigns.collapse_groups)
      |> assign(:is_admin, assigns.is_admin)}
   end
@@ -78,5 +104,45 @@ defmodule PhotoTaggerWeb.GalleryLive.GalleryPanel do
   def get_grid_size(zoom_level, base_size) do
     size = clamp(base_size - zoom_level, 1, 9)
     "grid-cols-#{size}"
+  end
+
+
+  defp paginate_photos(socket, new_page) when new_page >= 1 do
+    %{per_page: per_page, page: cur_page} = socket.assigns
+    photos = Enum.slice(socket.assigns.all_photos, ((new_page - 1) * per_page)..(new_page * per_page - 1))
+
+    {photos, at, limit} =
+      if new_page >= cur_page do
+        {photos, -1, per_page * 3 * -1}
+      else
+        {Enum.reverse(photos), 0, per_page * 3}
+      end
+
+    case photos do
+      [] ->
+        assign(socket, end_of_timeline?: at == -1)
+
+      [_ | _] = photos ->
+        socket
+        |> assign(end_of_timeline?: false)
+        |> assign(:page, new_page)
+        |> stream(:photos, photos, at: at, limit: limit)
+    end
+  end
+
+  def handle_event("next-page", _, socket) do
+    {:noreply, paginate_photos(socket, socket.assigns.page + 1)}
+  end
+
+  def handle_event("prev-page", %{"_overran" => true}, socket) do
+    {:noreply, paginate_photos(socket, 1)}
+  end
+
+  def handle_event("prev-page", _, socket) do
+    if socket.assigns.page > 1 do
+      {:noreply, paginate_photos(socket, socket.assigns.page - 1)}
+    else
+      {:noreply, socket}
+    end
   end
 end
