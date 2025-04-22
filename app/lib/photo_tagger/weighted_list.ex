ad58d95939ebcbd2b6aae9494197af009cd9cddb
tristan-orourke <tristan.orourke@gmail.com>
2025-04-21 22:41:25 -0400

    Indroduce drift page (it doesn't drift yet)

---
+++ app/lib/photo_tagger/weighted_list.ex
@@ -0,0 +1,19 @@
+defmodule PhotoTagger.WeightedList do
+  defstruct [:acc_list, :max]
+
+  # Initialize a weighted list where each item is a tuple of the form {value, weight}
+  def new(kw_list) do
+  # TODO: Might want to verify it is sorted lowest to highest weight
+  acc_list = Enum.scan(kw_list, fn {k, w}, {_, w_sum} -> {k, w + w_sum} end)
+  {_, max} = List.last(acc_list)
+  %__MODULE__{acc_list: acc_list, max: max}
+  end
+
+  def sample(%__MODULE__{acc_list: acc_list, max: max}) do
+    rand_value = Enum.random(1..max)
+    Enum.find_value(acc_list, fn
+      {k, w} when rand_value <= w -> k
+      _ -> false
+    end)
+  end
+end
---
+++ app/lib/photo_tagger_web/live/gallery_live/drift.ex
@@ -0,0 +1,76 @@
+defmodule PhotoTaggerWeb.GalleryLive.Drift do
+  use PhotoTaggerWeb, :live_view
+  require Logger
+
+  alias PhotoTagger.Repo
+  alias PhotoTagger.Gallery
+  alias PhotoTagger.WeightedList
+  alias PhotoTagger.Uploaders.ImageUploader
+
+  def render(assigns) do
+    ~H"""
+    <div class="fixed inset-0 overflow-y-auto w-screen h-screen flex items-center justify-center bg-zinc-800">
+      <img
+        class="object-contain w-full max-w-full lg:max-h-full"
+        alt={@photo.name}
+        src={ImageUploader.url({@photo.image, @photo}, :original)}
+      />
+      <div class="absolute top-4 left-4 text-sm">
+        <ul>
+          <li :for={tag <- @photo.tags} class="shadow-zinc-700/10 ring-zinc-800 shadow-2xl bg-white ring-1 md:ring-2 rounded-full p-1 m-2 md:p-2 w-min text-sm md:text-base">
+            #{tag.name}
+          </li>
+        </ul>
+      </div>
+    </div>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+  def handle_params(params, _session, socket) do
+    folder = Map.get(params, "folder", nil)
+    {:ok, photo_id} = Map.fetch(params, "photo_id")
+
+    photo = Gallery.get_photo!(String.to_integer(photo_id)) |> Repo.preload(:tags)
+
+    {:noreply, socket |> assign(:photo, photo) |> assign(:folder, folder)}
+  end
+
+  # NOTE: this is an expensive operation, and should be done in a background job
+  def pick_next_photo(photo, folder) do
+    # Get all photos in our selection, excluding the current one
+    photos =
+      case folder do
+        nil -> Gallery.list_photos()
+        folder -> Gallery.list_photos_in_folder(folder)
+      end
+      |> Enum.reject(&(&1.id == photo.id))
+      |> Repo.preload(:tags)
+
+    # Get the similarity scores for each photo, and build a weighted list
+    weighted_list =
+      photos
+      |> Enum.map(fn p -> {p, similarity_score(photo, p)} end)
+      |> WeightedList.new()
+
+    # Sample a photo from the weighted list
+    WeightedList.sample(weighted_list)
+  end
+
+  # One point for sharing the same folder. One point for each tag in common.
+  def similarity_score(photo1, photo2) do
+    folder_score = if photo1.folder == photo2.folder, do: 1, else: 0
+
+    tags1 = Enum.map(photo1.tags, & &1.id)
+    tags2 = Enum.map(photo2.tags, & &1.id)
+    tag_score =
+      tags1
+      |> Enum.filter(&(&1 in tags2))
+      |> length()
+
+    folder_score + tag_score
+  end
+end
