0361dd3c31b4a1346bf97b6cf58ffad1cc3ebaa9
tristan-orourke <tristan.orourke@gmail.com>
2025-08-07 23:15:28 -0400

    Update image variant sizes

--- app/lib/photo_tagger/uploaders/image_uploader.ex
+++ app/lib/photo_tagger/uploaders/image_uploader.ex
@@ -5,7 +5,7 @@ defmodule PhotoTagger.Uploaders.ImageUploader do
 
   require Logger
 
-  @versions [:original, :small, :thumb, :web]
+  @versions [:original, :small, :thumb, :web_lg, :web_md]
   @extensions ~w(.jpg .jpeg .gif .png)
 
   def valid_extensions, do: @extensions
@@ -27,12 +27,17 @@ defmodule PhotoTagger.Uploaders.ImageUploader do
 
   def transform(:thumb, _) do
     # Resize image to a maximum of 300x300, cropping to a square aspect ratio.
-    {:convert, "-strip -thumbnail 300x300^ -gravity center -extent 200x200 -format jpg", :jpg}
+    {:convert, "-strip -thumbnail 300x300^ -gravity center -extent 300x300 -format jpg", :jpg}
   end
 
-  def transform(:web, _) do
-    # Resizes to fit within 2500x2500, maintaining aspect ratio
-    {:convert, "-strip -resize 2500x2500 -format jpg", :jpg}
+  def transform(:web_md, _) do
+    # Resizes to fit within 600x600, maintaining aspect ratio
+    {:convert, "-strip -resize 600x600 -format jpg", :jpg}
+  end
+
+  def transform(:web_lg, _) do
+    # Resizes to fit within 1400x1400, maintaining aspect ratio
+    {:convert, "-strip -resize 1400x1400 -format jpg", :jpg}
   end
 
   # Override the persisted filenames:
--- app/make_thumb_and_web_images.sh
+++ app/make_thumb_and_web_images.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find /app/priv/static/uploads/images -type f ! -name "*.small.jpg" \
-  -exec bash -c 'magic "$0" -strip -resize 2500x2500 -format jpg "${0%.*}.web.jpg"' {} \;
+  -exec bash -c 'magic "$0" -strip -resize 1400x1400 -format jpg "${0%.*}.web.jpg"' {} \;
