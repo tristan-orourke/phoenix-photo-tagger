e1edf4c61e15928cd728f56266817aaa380b4b60
tristan-orourke <tristan.orourke@gmail.com>
2026-02-13 00:23:09 -0500

    Fix tabs->spaces in migration

--- app/priv/repo/migrations/20260210055910_add_original_photo_id_to_photos.exs
+++ app/priv/repo/migrations/20260210055910_add_original_photo_id_to_photos.exs
@@ -1,11 +1,11 @@
 defmodule PhotoTagger.Repo.Migrations.AddOriginalPhotoIdToPhotos do
-	use Ecto.Migration
+  use Ecto.Migration
 
-	def change do
-		alter table(:photos) do
-			add :original_photo_id, references(:photos, on_delete: :delete_all), null: true
-		end
+  def change do
+    alter table(:photos) do
+      add :original_photo_id, references(:photos, on_delete: :delete_all), null: true
+    end
 
-		create index(:photos, [:original_photo_id])
-	end
+    create index(:photos, [:original_photo_id])
+  end
 end
