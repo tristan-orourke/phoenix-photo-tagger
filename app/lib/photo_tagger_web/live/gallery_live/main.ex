25f29dc476d9974b694e8f9fdf37c351adb44202
tristan-orourke <tristan.orourke@gmail.com>
2026-02-12 23:42:11 -0500

    Avoid excluding cross-listings for now. Treat them as different photos

--- app/lib/photo_tagger_web/live/gallery_live/main.ex
+++ app/lib/photo_tagger_web/live/gallery_live/main.ex
@@ -290,21 +290,21 @@ defmodule PhotoTaggerWeb.GalleryLive.Main do
           prev_filtered_photos
 
         {nil, [], [], _} ->
-          Gallery.list_photos(include_private: is_admin, sort: sort, exclude_cross_listings: true)
+          Gallery.list_photos(include_private: is_admin, sort: sort, exclude_cross_listings: false)
 
         {nil, ["untagged"], _, _} ->
-          Gallery.list_photos_by_all_tags(nil, include_private: is_admin, sort: sort, exclude_cross_listings: true) ++
+          Gallery.list_photos_by_all_tags(nil, include_private: is_admin, sort: sort, exclude_cross_listings: false) ++
             Gallery.list_photos_by_tags(%{include: ["untagged"], exclude: []},
               include_private: is_admin,
               sort: sort,
-              exclude_cross_listings: true
+              exclude_cross_listings: false
             )
 
         {nil, tags, exclude_tags, _} ->
           Gallery.list_photos_by_tags(%{include: tags, exclude: exclude_tags},
             include_private: is_admin,
             sort: sort,
-            exclude_cross_listings: true
+            exclude_cross_listings: false
           )
 
         {folder, [], [], _} ->
